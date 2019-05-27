290393e0384df83e3c0d20f9d8f3b593b30ea983
Lucas Bonvin <lucasbonvin@hotmail.com>
2019-05-27 10:56:56 +0200

    added GenericThingConfiguration

---
+++ src/main/java/org/openhab/binding/mqtt/generic/internal/GenericThingConfiguration.java
@@ -0,0 +1,10 @@
+package org.openhab.binding.mqtt.generic.internal;
+
+import org.eclipse.jdt.annotation.NonNullByDefault;
+
+@NonNullByDefault
+public class GenericThingConfiguration {
+    public String cloudioEndpoint = "";
+    public String cloudioNode = "";
+
+}
